e690e787db38a7c3ee8bc6de63650cedff0c74d8
최우중 <chwj7700@naver.com>
2024-05-22 21:21:20 +0900

    pca 제거

--- main.py
+++ main.py
@@ -5,13 +5,45 @@ import seaborn as sns; sns.set()
 
 import naive as naive
 import logistic as logistic
-import pca as pca
+from sklearn.decomposition import PCA
 
-# Matplotlib에서 나눔 폰트 사용 설정
-plt.rc('font', family='NanumGothic')
+def pca(param_array):
 
-df = pd.read_csv('2학년1학기/datascience/content/공군_신체정보_남녀혼합.csv', encoding='cp949')
+    pca = PCA(n_components=1)
+    pca_array = pca.fit_transform(param_array)
 
-pca.run(df)
-naive.run(df)
-logistic.run(df)
+    # 시각화
+    #plt.figure(figsize=(8, 6))
+    #plt.scatter(pca_array, [0]*len(pca_array), alpha=0.5)
+    #plt.title('PCA')
+    #plt.xlabel('PCA')
+    #plt.yticks([])  # Y축 값 숨기기
+    #plt.show()
+
+    return pca_array;
+
+if __name__ == '__main__':
+    # Matplotlib에서 나눔 폰트 사용 설정
+    plt.rc('font', family='NanumGothic')
+
+    df = pd.read_csv('2학년1학기/data_science_teamplay/content/공군_신체정보_남녀혼합.csv', encoding='cp949')
+
+    df_man = df[df['성별'] == '남']
+    df_woman = df[df['성별'] == '여']
+    df_man = df_man.drop(['성별', '측정일자'], axis=1)
+    df_woman = df_woman.drop(['성별', '측정일자'], axis=1)
+
+    attribute = ['키', '몸무게', '머리둘레']
+    nd_man = df_man[attribute].values.reshape(-1, attribute.__len__())
+    nd_woman = df_woman[attribute].values.reshape(-1, attribute.__len__())
+
+    pca_man = pca(nd_man)
+    pca_woman = pca(nd_woman)
+
+    #sampleData = np.array([[175, 70], [160, 55]])
+    #pca.PCA.transform(sampleData)
+
+    naive.run(df)
+    logistic.run(df)
+
+
