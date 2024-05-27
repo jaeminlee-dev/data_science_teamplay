67f06deb5811710c5b77d912eae6b9a4b9e84232
Jaemin Lee <ljm005302@gmail.com>
2024-05-27 20:41:06 +0900

    Fix : pca refectoring

--- pca.py
+++ pca.py
@@ -4,23 +4,22 @@ import pandas as pd
 from sklearn.decomposition import PCA
 
 
-def run(df, sex='남'):
-    df_man = df[df['성별'] == sex]
-
-    df_man = df_man.drop(['성별', '측정일자'], axis=1)
-
-    남자키몸무게 = df_man[['키', '몸무게']].values.reshape(-1, 2)
-
+def run(df, sex='남', types=['키', '몸무게']):
+    df_filtered_sex = df[df['성별'] == sex]
+    df_reshaped = df_filtered_sex[types].values.reshape(-1, 2)
     pca = PCA(n_components=1)
-    남자키몸무게PCA = pca.fit_transform(남자키몸무게)
+    pca_data = pca.fit_transform(df_reshaped)
+    show(pca_data)
+    return pca_data
 
+def show(pca_data):
     # 시각화
     plt.figure(figsize=(8, 6))
-    plt.scatter(남자키몸무게PCA, [0]*len(남자키몸무게PCA), alpha=0.5)
-    plt.title('남자키몸무게PCA')
+    plt.scatter(pca_data, [0]*len(pca_data), alpha=0.5)
     plt.xlabel('PCA Component 1')
     plt.yticks([])  # Y축 값 숨기기
     plt.show()
+
 
 if __name__ == '__main__':
     plt.rc('font', family='AppleGothic')
