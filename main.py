31eb58d315fdaae1e5aa624d5f82fdf637868747
Jaemin Lee <ljm005302@gmail.com>
2024-05-27 19:41:06 +0900

    Fix : 엑셀 데이터 상대경로로 수정

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+from sklearn.decomposition import PCA
+import logistic as logistic
+import naive as naive
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from pca import pca; sns.set()
-
-import naive as naive
-import logistic as logistic
-from sklearn.decomposition import PCA
+from pca import pca
+sns.set()
 
 
 if __name__ == '__main__':
     # Matplotlib에서 나눔 폰트 사용 설정
     plt.rc('font', family='NanumGothic')
 
-    df = pd.read_csv('2학년1학기/data_science_teamplay/content/공군_신체정보_남녀혼합.csv', encoding='cp949')
+    df = pd.read_csv(
+        './content/공군_신체정보_남녀혼합.csv', encoding='cp949')
 
     df_man = df[df['성별'] == '남']
     df_woman = df[df['성별'] == '여']
@@ -28,10 +29,8 @@ if __name__ == '__main__':
     pca_man = pca(nd_man)
     pca_woman = pca(nd_woman)
 
-    #sampleData = np.array([[175, 70], [160, 55]])
-    #pca.PCA.transform(sampleData)
+    # sampleData = np.array([[175, 70], [160, 55]])
+    # pca.PCA.transform(sampleData)
 
     naive.run(df)
     logistic.run(df)
-
-
