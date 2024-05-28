144ccce6cf156d9d55ce7d10fb56f3dec3a33d36
Jaemin Lee <ljm005302@gmail.com>
2024-05-28 22:07:10 +0900

    Fix : lda 리펙터링

--- lda.py
+++ lda.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 
 def run(df, types=['키', '몸무게']):
     scaled = StandardScaler().fit_transform(df[types])
-    lda = LinearDiscriminantAnalysis(n_components=1)
-
     target = df['성별'].replace({"남": 0, "여": 1}).values
+
+    lda = LinearDiscriminantAnalysis(n_components=1)
     lda.fit(scaled, target)
     lda_data = lda.transform(scaled)
+    show(lda_data, target)
 
+
+def show(lda_data, target):
     markers = ['^', 's']
     colors = ['blue', 'green']
     target_names = ['man', 'girl']
@@ -23,15 +26,6 @@ def run(df, types=['키', '몸무게']):
     plt.legend(loc='upper right')
     plt.xlabel('lda_component_1')
     # plt.ylabel('lda_component_2')
-    plt.show()
-
-
-def show(pca_data):
-    # 시각화
-    plt.figure(figsize=(8, 6))
-    plt.scatter(pca_data, [0]*len(pca_data), alpha=0.5)
-    plt.xlabel('PCA Component 1')
-    plt.yticks([])  # Y축 값 숨기기
     plt.show()
 
 
