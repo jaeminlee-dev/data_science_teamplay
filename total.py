972d677bc50b1d10be0b393b8fccd38b917a9da6
Jaemin Lee <ljm005302@gmail.com>
2024-06-12 12:09:46 +0900

    Add : total

---
+++ total.py
@@ -0,0 +1,9 @@
+# Load a Korean font for proper display of Hangul characters
+from matplotlib import pyplot as plt
+import matplotlib.font_manager as fm
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('content/총합 치수 모델 정확도.csv')
+
